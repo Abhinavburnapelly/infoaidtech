9ee6dcc35af490342302f7e606b1c0fe470ddd1e
Abhinavburnapelly <129979245+Abhinavburnapelly@users.noreply.github.com>
2023-08-20 16:08:16 +0530

    Update todo.py

--- python project intern/to do list/todo.py
+++ python project intern/to do list/todo.py
@@ -26,36 +26,47 @@ def delete():
 root=Tk()
 root.title('To do list')
 root.config(bg='#e2deff')
-root.geometry("500x500")
+root.geometry("500x560")
 root.resizable(False,False)
 
 
 
-frame=Frame(root,bg='#118fee')
-frame.pack(side=TOP)
-
+frame=Frame(root,bg='#118fee',highlightbackground="blue", highlightthickness=2)
+frame.pack(side=TOP,pady=5)
+frame.configure(width=30)
 label1=Label(frame,text='Enter a task',font="bold 15")
 label1.pack(side=TOP,pady=10)
 
 # text
-text=Entry(frame,width=10,textvariable=StringVar(),font=('Arial 24'))
+text=Entry(frame,width=10,textvariable=StringVar(),font=('Arial 20'))
 text.pack(side=LEFT,padx=10,pady=5)
 
 submit_button=Button(frame,text='Submit',font="cosmicsms 15 normal",command=submit)
 submit_button.pack(side=LEFT,padx=10,pady=5)
 
-remove_allb=Button(frame,text='Clear',font="cosmicsms 15 normal",command=remove_all)
-remove_allb.pack(side=LEFT,padx=10,pady=5)
+
 
 delete_button=Button(frame,text='Delete',font="cosmicsms 15 normal",command=delete)
 delete_button.pack(side=RIGHT,padx=10,pady=5)
 # frame 2
 
-frame2=Frame(root)
-frame2.pack(side=TOP,pady=20)
-task_list=Listbox(frame2,font=('arial 20'),selectmode=MULTIPLE)
+frame2=Frame(root,highlightbackground="grey", highlightthickness=3)
+frame2.pack(side=TOP,pady=10)
+task_list=Listbox(frame2,font=('cosmicsms 20'),selectmode=MULTIPLE)
+task_list.configure(width=30,)
+task_list.pack(side=TOP,fill=X, expand=YES)
 
 
-task_list.pack(side=TOP)
+# frame 3
+frame3=Frame(root,highlightbackground="black", highlightthickness=1)
+
+clear_img=PhotoImage(file='clear all.png')
+
+remove_allb=Button(frame3,image=clear_img,font="cosmicsms 15 normal",command=remove_all)
+remove_allb.pack(side=TOP,padx=10)
+clear_all=Label(frame3,text='clear all',font="cosmicsms 10 normal")
+clear_all.pack(side=BOTTOM,pady=5)
+
+frame3.pack(side=TOP)
 
 root.mainloop()
